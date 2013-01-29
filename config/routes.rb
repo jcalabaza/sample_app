364f2cdfa97e2517d65d3efb1d29e69d6384896c
John Thomas <john@andyouknowthis.net>
2013-01-29 14:13:39 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 SampleApp::Application.routes.draw do
 
-  get "monkey_cat/libby"
-
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
